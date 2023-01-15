89f4e4cec52c677db8f9babb9707b5a387ffd6de
Michal Radomski <michalra@op.pl>
2023-01-15 18:25:02 +0100

    Client - initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./App.scss";
+
+function App(): JSX.Element {
+  return <React.Fragment>PERN Stack App</React.Fragment>;
+}
+
+export default App;
---
+++ client/src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import { Provider } from "react-redux";
+
+// import store from "./redux/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
+  </React.StrictMode>
+);
