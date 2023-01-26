fbc2b7080795f7a2045237670fc01c5a848057a4
Michal Radomski <michalra@op.pl>
2023-01-26 01:10:49 +0100

    Login - commit2

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -22,9 +22,17 @@ const Login = (): JSX.Element => {
 
   const { email, password } = loginInputs;
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {};
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    // console.log({ name, value });
+    setLoginInputs((values) => ({ ...values, [name]: value }));
+  };
 
-  const onSubmitForm = async (event: React.SyntheticEvent) => {};
+  const onSubmitForm = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    alert(`${email} ${password}`);
+  };
 
   return (
     <React.Fragment>
@@ -33,11 +41,13 @@ const Login = (): JSX.Element => {
         <form onSubmit={onSubmitForm}>
           <input
             placeholder="email"
-            type="text"
+            type="email"
             name="email"
             value={email}
             onChange={(event) => onChange(event)}
             className="form-control my-3"
+            minLength={6}
+            required={true}
           />
           <input
             placeholder="password"
@@ -46,6 +56,8 @@ const Login = (): JSX.Element => {
             value={password}
             onChange={(event) => onChange(event)}
             className="form-control my-3"
+            minLength={8}
+            required={true}
           />
           <button className="btn btn-success" style={{ width: "100%" }}>
             Submit
