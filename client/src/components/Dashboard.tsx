5a12f61d7206f7bd539fa3bfca47b44e8df7d2c6
Michal Radomski <michalra@op.pl>
2023-02-02 02:17:34 +0100

    Dashboard - initial commit

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,9 +1,34 @@
+import axios from "axios";
 import React from "react";
 
 const Dashboard = (): JSX.Element => {
+  const [privateState, setPrivateState] = React.useState<string>("");
+
+  React.useEffect(() => {
+    const URL = "/api/dashboard";
+    axios
+      .get(URL)
+      .then((response) => {
+        const dataToPass = response?.data;
+        // console.log("response.status:", response.status);
+        // console.log("dataToPass:", dataToPass);
+        if (response.status === 200) {
+          setPrivateState(dataToPass.message);
+        }
+      })
+      .catch(function (error) {
+        if (error) {
+          console.log({ error });
+        }
+      });
+  }, []);
+
   return (
     <React.Fragment>
       <h1 style={{ textAlign: "center", marginTop: "80px" }}>Welcome to the Dashboard of the PERN Stack Auth App</h1>
+      <h3 style={{ textAlign: "center" }}>
+        message: <span style={{ fontWeight: "bold" }}>{privateState}</span>
+      </h3>
     </React.Fragment>
   );
 };
