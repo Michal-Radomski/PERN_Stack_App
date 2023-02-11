50332a5e5afd1d7c8ae108d648d9dc55d862f97c
Michal Radomski <michalra@op.pl>
2023-02-11 15:08:46 +0100

    Time converter updated

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -1,8 +1,23 @@
 export const timestampToString = (timestamp: number): string => {
-  const dateString = new Date(timestamp * 1000).toISOString().replace("T", ", ");
-  const dateArray = dateString.split(".");
-  const dateToReturn = dateArray[0];
-  return dateToReturn;
+  const dateString = new Date(timestamp * 1000);
+  function padTo2Digits(num: number) {
+    return num.toString().padStart(2, "0");
+  }
+
+  function formatDate(date: Date) {
+    return [date.getFullYear(), padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate())].join("-");
+  }
+
+  function formatTime(date: Date) {
+    return [padTo2Digits(date.getHours()), padTo2Digits(date.getMinutes()), padTo2Digits(date.getSeconds())].join(":");
+  }
+
+  const dateToExport = formatDate(dateString);
+  const timeToExport = formatTime(dateString);
+
+  const dateTimeString = `${dateToExport}, ${timeToExport}`;
+  // console.log("dateTimeString:", dateTimeString);
+  return dateTimeString;
 };
 
 export const timeStringRefactor = (timeString: string): string => {
